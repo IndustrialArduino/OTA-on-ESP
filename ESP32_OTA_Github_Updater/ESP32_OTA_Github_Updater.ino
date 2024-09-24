8b79be1eb9aea2028de177ee8aa6c7f6bea400ac
Pasan Erathna <167863121+Pasan1021@users.noreply.github.com>
2024-09-24 15:21:00 +0530

    Update ESP32_OTA_Github_Updater.ino

--- ESP32_OTA_Github_Updater/ESP32_OTA_Github_Updater.ino
+++ ESP32_OTA_Github_Updater/ESP32_OTA_Github_Updater.ino
@@ -158,7 +158,7 @@ bool checkForUpdate(String &firmware_url) {
 
     if (new_version != current_version) {
       Serial.println("New version available. Updating...");
-      firmware_url = String("https://raw.githubusercontent.com/IndustrialArduino/OTA-on-ESP/release/firmware_v")+ String("firmware_v")+ new_version + String(".bin");
+      firmware_url = String("https://raw.githubusercontent.com/IndustrialArduino/OTA-on-ESP/release/firmware_v")+ new_version + String(".bin");
       Serial.println("Firmware URL: " + firmware_url);
       return true;
     } else {
